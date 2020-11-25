a6b020103238bd0c6b652496a8624a46e2384959
youssefbenjaber <youssefbenjaber@yahoo.com>
2020-11-25 09:16:48 +0100

    project

---
+++ app/src/main/java/com/mdw/covidstats/recherche.java
@@ -0,0 +1,14 @@
+package com.mdw.covidstats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recherche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recherche);
+    }
+}
