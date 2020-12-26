74ca3f9793acdc27a7876676350ddaa8403f5ae2
adam gharsallah <Adamgharsallah2@gmail.com>
2020-12-26 19:01:59 +0100

    choice page

--- app/src/main/java/com/mdw/covidstats/ChoiceActivity.java
+++ app/src/main/java/com/mdw/covidstats/ChoiceActivity.java
@@ -1,4 +1,33 @@
 package com.mdw.covidstats;
 
-public class ChoiceActivity {
-}
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
+
+    public class ChoiceActivity extends AppCompatActivity {
+        Button btn_1, btn_2;
+
+        @Override
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_choice);
+            btn_1 = findViewById(R.id.btn_1);
+            btn_1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View V) {
+                    startActivity(new Intent(getApplicationContext(), QuizQuestionsActivity.class));
+                }
+            });
+
+
+        }
+    }
